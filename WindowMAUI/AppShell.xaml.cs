dd0dab37e895a4b35c0a08b2e9f066d660f9e9e5
sc <sc@DESKTOP-UD03Q2M>
2022-06-26 12:20:36 +0200

    Add project files.

---
+++ WindowMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WindowMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ WindowMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WindowMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
